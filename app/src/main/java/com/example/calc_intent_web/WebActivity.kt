a644b3f33b2a712b3f0f8fe8f6182febed1e7b5a
Blissmal <malutibethuel@gmail.com>
2023-04-14 09:47:56 +0300

    Updates

--- app/src/main/java/com/example/calc_intent_web/WebActivity.kt
+++ app/src/main/java/com/example/calc_intent_web/WebActivity.kt
@@ -2,10 +2,23 @@ package com.example.calc_intent_web
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class WebActivity : AppCompatActivity() {
+    lateinit var mywebview: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        mywebview = findViewById(R.id.webView)
+        webview()
+    }
+    private fun webview(){
+        mywebview.webViewClient = WebViewClient()
+        mywebview.apply {
+            loadUrl("https://www.instagram.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
     }
 }
