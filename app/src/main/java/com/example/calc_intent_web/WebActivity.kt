f2c07f44e2a4aef672b640afffd8a2e25bde90b4
Blissmal <malutibethuel@gmail.com>
2023-04-14 10:21:14 +0300

    Update

--- app/src/main/java/com/example/calc_intent_web/WebActivity.kt
+++ app/src/main/java/com/example/calc_intent_web/WebActivity.kt
@@ -1,18 +1,22 @@
 package com.example.calc_intent_web
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class WebActivity : AppCompatActivity() {
     lateinit var mywebview: WebView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
         mywebview = findViewById(R.id.webView)
         webview()
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun webview(){
         mywebview.webViewClient = WebViewClient()
         mywebview.apply {
@@ -20,5 +24,10 @@ class WebActivity : AppCompatActivity() {
             settings.javaScriptEnabled = true
             settings.safeBrowsingEnabled = true
         }
+    }
+
+    override fun onBackPressed() {
+        if (mywebview.canGoBack())mywebview.goBack()else
+        super.onBackPressed()
     }
 }
